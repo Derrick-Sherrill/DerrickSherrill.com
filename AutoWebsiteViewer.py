0840905e43c3d22fdd8ae25c9d37d0790d2c9e21
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2018-04-20 19:50:35 -0400

    Create AutoWebsiteViewer.py

---
+++ AutoWebsiteViewer.py
@@ -0,0 +1,17 @@
+import webbrowser
+import time
+from pykeyboard import PyKeyboard
+
+count = 0
+urls = ['https://pypi.org/project/pyobjc-framework-Quartz/','https://github.com/SavinaRoja/PyKeyboard','http://www.derricksherrill.com/django/multiple-database-query/']
+k = PyKeyboard()
+
+while count < 100:
+    for url in urls:
+        webbrowser.open(url, new=0)
+        time.sleep(10)
+        k.press_keys(['Command','W'])
+        count = count + 1 
+    
+else:
+    pass
