c0d989e9f80efb803874d4be2e256cf882a19a17
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-04-18 22:32:53 -0400

    create python_numbers.py
    Script used in python numbers tutorial

---
+++ python_numbers.py
@@ -0,0 +1,27 @@
+import math
+
+A = -10
+B = 5.5
+C = 4 + 6j
+
+#Absolute Value
+print(math.fabs(A))
+# e^x
+print(math.exp(B))
+# Natural Log
+print(math.log(1))
+#Base Ten log
+print(math.log10(1000))
+#Sqrt
+print(math.sqrt(4))
+
+ListA = [10,20,30]
+print(max(ListA))
+print(min(ListA))
+
+print(math.cos(1))
+print(math.sin(1))
+print(math.tan(1))
+print(math.hypot(3,4))
+
+print(math.degrees(math.sin(1)))
