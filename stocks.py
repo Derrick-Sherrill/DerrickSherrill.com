500bbfd0bc6f8ac403863b3dd26c5dc69550e27b
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-06-08 10:58:19 +0100

    For my Real Time Stock Market Data Analysis video

---
+++ stocks.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+api_key = 'RNZPXZ6Q9FEFMEHM'
+
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT', interval = '1min', outputsize = 'full')
+print(data)
+
+i = 1
+#while i==1:
+#    data, meta_data = ts.get_intraday(symbol='MSFT', interval = '1min', outputsize = 'full')
+#    data.to_excel("output.xlsx")
+#    time.sleep(60)
+
+close_data = data['4. close']
+percentage_change = close_data.pct_change()
+
+print(percentage_change)
+
+last_change = percentage_change[-1]
+
+if abs(last_change) > 0.0004:
+    print("MSFT Alert:" + last_change)
